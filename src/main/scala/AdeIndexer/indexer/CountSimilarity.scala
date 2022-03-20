16bb587a4a4de66c5ccf156f11bcb4d0e091a47e
Georvic Tur <alexanderstower@gmail.com>
2022-03-20 01:47:57 +0100

    comments added to the custom similarity

--- src/main/scala/AdeIndexer/indexer/CountSimilarity.scala
+++ src/main/scala/AdeIndexer/indexer/CountSimilarity.scala
@@ -1,16 +1,26 @@
 package AdeIndexer.indexer
+/** Custom similarity for conducting simple counts.
+ * */
+
 
 import org.apache.lucene.search.similarities.{BasicStats, SimilarityBase}
 
 import java.util.logging.Logger
 
-
+/** A custom Similarity to count the number of appearances of single terms in the inverted index.
+ * See [[org.apache.lucene.search.similarities.SimilarityBase]].
+ * */
 class CountSimilarity extends SimilarityBase {
 
   private val logger = Logger.getLogger(this.getClass.getName)
 
+  /** Return the number of times a term has occurred in a document.
+   * @param basicStats: See [[org.apache.lucene.search.similarities.BasicStats]].
+   * @param v: the number of times a term has occurred in a document.
+   * @param v1: the document length.
+   * */
   override def score(basicStats: BasicStats, v: Double, v1: Double): Double = {
-    logger.fine(s"CountSimilarity v: ${v} v1: ${v1}")
+    logger.fine(s"CountSimilarity v: $v v1: $v1")
     v
   }
 
--- src/main/scala/AdeIndexer/postprocessing/Scaler.scala
+++ src/main/scala/AdeIndexer/postprocessing/Scaler.scala
@@ -1,6 +1,16 @@
 package AdeIndexer.postprocessing
+/** Utilities for rescaling Lucene scores.
+ * */
 
+
+/** A scaler for the scores returned by Lucene.
+ * */
 object Scaler {
+
+  /** Rescale the max score into 100 and the min to 0.
+   * @param scoredDocs: a map of identifier -> score.
+   * @return the same map, but with rescaled scores.
+   * */
   def rescaleScores(scoredDocs: Map[String, Float]): Map[String, Float] = {
     val maxValue = scoredDocs.maxBy(_._2)._2
     scoredDocs.map(
