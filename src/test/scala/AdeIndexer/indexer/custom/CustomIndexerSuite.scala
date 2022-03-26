c671555e5e6d8540f381d15db44761faf0a2fd72
J0hnG4lt <georvic.tur@gmail.com>
2022-03-27 00:32:50 +0100

    unit test for custom indexer has been fixed

--- src/test/scala/AdeIndexer/indexer/custom/CustomIndexerSuite.scala
+++ src/test/scala/AdeIndexer/indexer/custom/CustomIndexerSuite.scala
@@ -9,28 +9,30 @@ import java.nio.file.Paths
 class CustomIndexerSuite extends AnyFunSuite {
   val folder = Paths.get("src", "test", "resources")
   val testTokens = Map("Georvic"->1, "Tur"->2, "Victoria"->1, "Jorge"->1, "Orlene"->1)
+  val indexer = CustomIndexer(folder=folder)
+  val filePaths = indexer.getFilePaths()
+  val documents = indexer.loadDocuments()
+  val invertedIndex = indexer.loadIndex()
+
 
   test("CustomIndexer should find all text documents") {
-
-    val indexer = CustomIndexer(folder=folder)
-    val filePaths = indexer.getFilePaths()
     assert(filePaths.size == 3, "Unexpected number of documents.")
+  }
+
+  test("documents in CustomIndexer should not be empty") {
+    assert(documents.size > 0, "We expected at least one document.")
+  }
+
+  test("invertedIndex in CustomIndexer should not be empty") {
+    assert(invertedIndex.size > 0, "We expected at least one indexed word.")
   }
 
   testTokens.foreach {
     case (testToken, appearances) => {
 
       test(s"The indexer should index $testToken in $appearances documents.") {
-        val indexer = CustomIndexer(folder = folder)
-        val documents = indexer.loadDocuments()
-
-        assert(documents.size > 0, "We expected at least one document.")
-
-        val invertedIndex = indexer.loadIndex()
-
-        assert(invertedIndex.size == appearances, "We expected at least one indexed word.")
-
         val testDocuments = invertedIndex.apply(testToken)
+        assert(testDocuments.size == appearances, "We expected at least one indexed word.")
         testDocuments.foreach(
           documentId => {
             val documentPath = documents.apply(documentId)
@@ -41,7 +43,6 @@ class CustomIndexerSuite extends AnyFunSuite {
               contents.contains(testToken),
               s"The document $documentFilepath does not contain the test token $testToken. File Contents: $contents"
             )
-
           }
         )
       }
